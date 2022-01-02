16a096193bce6b8564e3590d710c1293dd36ad2f
LuminiNanayakkara <68697851+LuminiNanayakkara@users.noreply.github.com>
2022-01-02 22:57:27 +0530

    Error Fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { AddUser } from "./components/AddUser";
 import { EditUser } from "./components/EditUser";
@@ -6,9 +7,13 @@ import { EditUser } from "./components/EditUser";
 function App() {
   return (
     <div className="App">
-      <Home />
-      <AddUser />
-      <EditUser />
+      <Router>
+        <h1>Nav</h1>
+
+        <Route path="/" element={<Home />} />
+        <AddUser />
+        <EditUser />
+      </Router>
     </div>
   );
 }
